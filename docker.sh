2e589f4d80c488997cdcbbb3dee40b8fb28bdd45
Tohid Hanfi <119034342+tohidhanfi20@users.noreply.github.com>
2024-05-20 13:15:57 +0530

    3rd command is updated

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
